97454151f2bd603994357bd7d5c07c4bfdcfb64c
Jireh Battung <jirehgbattung@gmail.com>
2024-09-14 17:52:36 +0800

    Add sidebar menu links to utils

---
+++ src/components/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as FilledWalletIcon } from './FilledWalletIcon';
+export { default as FilledCircleMinusIcon } from './FilledCircleMinus'
+export { default as FilledPigMoneyIcon } from './FilledPigMoney';
--- src/utils/Sidebar/sidebarUtils.tsx
+++ src/utils/Sidebar/sidebarUtils.tsx
@@ -1,4 +1,5 @@
-import { IconProps, Icon, IconCirclePlusFilled, IconCircleRectangleFilled, IconCircleArrowUpRightFilled, IconCircleArrowRightFilled } from "@tabler/icons-react";
+import { FilledWalletIcon, FilledCircleMinusIcon, FilledPigMoneyIcon } from "@/components/icons";
+import { IconProps, Icon, IconCirclePlusFilled, IconCircleArrowUpRightFilled, IconCircleArrowRightFilled, IconHomeFilled, IconSwitchHorizontal, IconTargetArrow, IconGraph, IconRefresh, IconCreditCardFilled } from "@tabler/icons-react";
 import { ForwardRefExoticComponent, RefAttributes } from "react";
 
 export type ISidebarQuickActions = {
@@ -15,7 +16,7 @@ export const sidebarQuickActions: ISidebarQuickActions[] = [
   },
   {
     buttonId: 'add-expense',
-    Icon: IconCircleRectangleFilled,
+    Icon: FilledCircleMinusIcon,
     text: 'Add Expense',
   },
   {
@@ -27,5 +28,63 @@ export const sidebarQuickActions: ISidebarQuickActions[] = [
     buttonId: 'add-to-goal',
     Icon: IconCircleArrowRightFilled,
     text: 'Add to Goal',
+  },
+];
+
+export type ISidebarMainMenuLinks = {
+  buttonId: string;
+  route: string;
+  Icon: ForwardRefExoticComponent<IconProps & RefAttributes<Icon>>;
+  text: string;
+};
+
+export const sidebarMainMenuLinks: ISidebarMainMenuLinks[] = [
+  {
+    buttonId: 'dashboard',
+    route: '/',
+    Icon: IconHomeFilled,
+    text: 'Dashboard'
+  },
+  {
+    buttonId: 'accounts',
+    route: '/accounts',
+    Icon: FilledWalletIcon,
+    text: 'Accounts'
+  },
+  {
+    buttonId: 'transactions',
+    route: '/transactions',
+    Icon: IconSwitchHorizontal,
+    text: 'Transactions'
+  },
+  {
+    buttonId: 'budgets',
+    route: '/budgets',
+    Icon: FilledPigMoneyIcon,
+    text: 'Budgets'
+  },
+  {
+    buttonId: 'targets',
+    route: '/targets',
+    Icon: IconTargetArrow,
+    text: 'targets'
+  },
+  {
+    buttonId: 'reports',
+    route: '/reports',
+    Icon: IconGraph,
+    text: 'Reports'
+  },
+  {
+    buttonId: 'subscriptions',
+    route: '/subscriptions',
+    Icon: IconRefresh,
+    text: 'Subscriptions'
+  },
+  {
+    buttonId: 'cards',
+    route: '/cards',
+    Icon: IconCreditCardFilled,
+    text: 'Cards'
   },
 ];
