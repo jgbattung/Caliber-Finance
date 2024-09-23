16df83e24433b86a764dec0a8d4e8fef7f3d7871
Jireh Battung <jirehgbattung@gmail.com>
2024-09-23 18:15:27 +0800

    Create Header tests

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,19 +2,26 @@ import React from 'react'
 import "./../../app/globals.css";
 import { IconUserCircle } from '@tabler/icons-react';
 import NotificationBell from '../NotificationBell/NotificationBell';
+import { headerTestIds } from '@/utils/constants';
 
 const Header = () => {
   const notificationCount: number = 2;
 
   return (
-    <div className='flex items-center justify-between w-full px-4 py-4 border-b md:px-6 max-md:h-16 lg:hidden text-color-primary border-b-bg-dark'>
+    <div
+      role='banner'
+      data-testid={headerTestIds.header}
+      className='flex items-center justify-between w-full px-4 py-4 border-b md:px-6 max-md:h-16 lg:hidden text-color-primary border-b-bg-dark'>
       <div className='flex flex-col md:flex-row md:items-end md:justify-center md:gap-1'>
         <p className='font-normal text-body-xs md:text-body-sm'>Good evening,</p>
         <p className='max-md:-mt-1.5 font-bold text-body-sm md:text-body'>Jireh John</p>
       </div>
       <div className='flex gap-3 md:gap-5 text-color-primary'>
-        <NotificationBell count={notificationCount} />
+        <NotificationBell
+          count={notificationCount}
+        />
         <IconUserCircle
+          data-testid={headerTestIds.userProfileIcon}
           size={28}
           stroke={1}
         />
