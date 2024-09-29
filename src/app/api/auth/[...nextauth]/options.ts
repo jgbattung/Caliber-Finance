4a882c2e9ef81767f51571b4328a610f9a425ec4
Jireh Battung <jirehgbattung@gmail.com>
2024-09-29 17:49:05 +0800

    Initial setup for email auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,6 @@
 import GoogleProvider from "next-auth/providers/google"
 import FacebookProvider from "next-auth/providers/facebook"
+import EmailProvider from "next-auth/providers/email"
 import { NextAuthOptions } from "next-auth"
 
 export const authOptions: NextAuthOptions = {
@@ -11,6 +12,17 @@ export const authOptions: NextAuthOptions = {
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID!,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
     }),
   ],
   session: {
