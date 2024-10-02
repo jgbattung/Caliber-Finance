82845a814f6a34035742f1615d1b59669f048957
Jireh Battung <jirehgbattung@gmail.com>
2024-10-02 20:19:55 +0800

    Create landing screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
+import Image from 'next/image'
+import React from 'react'
+import Logo from '../public/assets/logo.png'
+import Link from 'next/link'
 
-
-export default function Home() {
+const Landing = () => {
   return (
-    <h1>Start here</h1>
-  );
+    <div className='flex flex-col items-center justify-around h-dvh'>
+      <div className='flex flex-col items-center justify-center gap-2'>
+        <Image 
+          src={Logo}
+          alt='Caliber logo'
+          width={200}
+          height={90}
+        />
+        <p className='text-body-xs'>
+          Precision finance for a powerful future
+        </p>
+      </div>
+      <div className='flex flex-col items-center justify-center gap-3 font-montserrat'>
+        <Link
+          href='/sign-in'
+          className='flex items-center justify-center py-2 transition-all border rounded-md w-72 hover:bg-primary-600 bg-primary-500 border-primary-500'
+        >
+          <p className='font-semibold text-light-100'>
+            Login
+          </p>
+        </Link>
+        <Link
+          href='/sign-up'
+          className='flex items-center justify-center py-2 border-2 rounded-md hover:bg-slate-500/[0.2] transition-all w-72 border-primary-500' 
+        >
+          <p className='font-semibold text-primary-500'>
+            Register
+          </p>
+        </Link>
+      </div>
+    </div>
+  )
 }
+
+export default Landing
